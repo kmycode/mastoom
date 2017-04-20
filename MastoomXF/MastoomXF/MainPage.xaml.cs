4e15153ed9206f95f4e36bdeaabf6413662095c2
amay077 <okuokuoku@gmail.com>
2017-04-20 21:45:05 +0900

    名前空間を Mastoom に変更

---
+++ MastoomXF/MastoomXF/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Mastoom
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
