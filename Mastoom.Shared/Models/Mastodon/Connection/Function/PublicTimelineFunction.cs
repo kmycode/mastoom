82e8fc4683fa2fea8471ac3fd5bdc1acb93528db
KMY <tt@kmycode.net>
2017-04-22 10:39:45 +0900

    公開タイムライン取得機能を抽象化

--- Mastoom.Shared/Models/Mastodon/Connection/Function/PublicTimelineFunction.cs
+++ Mastoom.Shared/Models/Mastodon/Connection/Function/PublicTimelineFunction.cs
@@ -11,124 +11,18 @@ namespace Mastoom.Shared.Models.Mastodon.Connection.Function
     /// <summary>
     /// 公開タイムラインのファンクション
     /// </summary>
-    class PublicTimelineFunction : IConnectionFunction<MastodonStatus>
+    class PublicTimelineFunction : TimelineFunctionBase
     {
-        public MastodonClient Client { private get; set; }
-        public string StreamingInstanceUri { get; set; }
-
-        private TimelineStreaming streaming;
-        private bool isStreaming;
-
-        private const int StreamingTimeOut = 10;    // 一定時間ストリーミングの更新がない時に接続し直す（バグ？）
-        private Timer timer;
-
-        public event EventHandler<ObjectFunctionUpdateEventArgs<MastodonStatus>> Updated;
-        public event EventHandler<ObjectFunctionEventArgs<MastodonStatus>> Deleted;
-        public event EventHandler<ObjectFunctionErrorEventArgs> Errored;
-
-        public PublicTimelineFunction()
+        protected override TimelineStreaming GetTimelineStreaming(string streamInstanceUri = null)
         {
-            // ストリーミングの更新がタイムアウトした時の処理
-            this.timer = new Timer(async (sender) =>
+            if (string.IsNullOrEmpty(streamInstanceUri))
             {
-                await this.StopAsync();
-                await this.StartAsync();
-            }, null, Timeout.Infinite, Timeout.Infinite);
-        }
-
-        public async Task GetNewerAsync()
-        {
-        }
-
-        public async Task GetPrevAsync()
-        {
-        }
-
-        public async Task StartAsync()
-        {
-            await this.StartAsync(1);
-        }
-
-        private async Task StartAsync(int tryCount, Exception lastException = null)
-        {
-            if (this.isStreaming)
-            {
-                return;
+                return this.Client.GetPublicStreaming();
             }
-
-            // 試行回数
-            if (tryCount > 3)
+            else
             {
-                this.Errored?.Invoke(this, new ObjectFunctionErrorEventArgs
-                {
-                    Exception = lastException,
-                    Type = ObjectFunctionErrorType.StartError,
-                });
-                return;
+                return this.Client.GetPublicStreaming(this.StreamingInstanceUri);
             }
-
-            try
-            {
-                if (this.streaming == null)
-                {
-                    if (this.StreamingInstanceUri == null)
-                    {
-                        this.streaming = this.Client.GetPublicStreaming();
-                    }
-                    else
-                    {
-                        this.streaming = this.Client.GetPublicStreaming(this.StreamingInstanceUri);
-                    }
-
-                    this.streaming.OnUpdate += this.Streaming_OnUpdate;
-                    this.streaming.OnDelete += this.Streaming_OnDelete;
-                }
-
-                this.timer.Change(1000 * StreamingTimeOut, Timeout.Infinite);
-                this.streaming.Start();
-
-                this.isStreaming = true;
-            }
-            catch (Exception e)
-            {
-                await this.StopAsync();
-                await this.StartAsync(tryCount + 1, e);
-            }
-        }
-
-        public async Task StopAsync()
-        {
-            if (this.isStreaming)
-            {
-                try
-                {
-                    this.streaming.Stop();
-                }
-                catch { }
-                this.streaming.OnUpdate -= this.Streaming_OnUpdate;
-                this.streaming.OnDelete -= this.Streaming_OnDelete;
-                this.streaming = null;
-                this.timer.Change(Timeout.Infinite, Timeout.Infinite);
-
-                this.isStreaming = false;
-            }
-        }
-
-        private void Streaming_OnUpdate(object sender, StreamUpdateEventArgs e)
-        {
-            this.timer.Change(1000 * StreamingTimeOut, Timeout.Infinite);
-            this.Updated?.Invoke(this, new ObjectFunctionUpdateEventArgs<MastodonStatus>
-            {
-                Object = new MastodonStatus(e.Status),
-            });
-        }
-
-        private void Streaming_OnDelete(object sender, StreamDeleteEventArgs e)
-        {
-            this.Deleted?.Invoke(this, new ObjectFunctionEventArgs<MastodonStatus>
-            {
-                Object = new MastodonStatus(e.StatusId, null),
-            });
         }
     }
 }
