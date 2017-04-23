48a279a29d68fbc97a536793e3a136a1d2e6c481
amay077 <okuokuoku@gmail.com>
2017-04-24 03:01:04 +0900

    「ｎ件の新着」を実装

--- MastoomXF/MastoomXF/Behaviors/BindingContextPassingBehavior.cs
+++ MastoomXF/MastoomXF/Behaviors/BindingContextPassingBehavior.cs
@@ -3,33 +3,33 @@ using Xamarin.Forms;
 
 namespace Mastoom.Behaviors
 {
-    public class BindingContextPassingBehavior<T> : Behavior<T> where T : BindableObject
-    {
-        public T AssociatedObject { get; private set; }
-        
-        protected override void OnAttachedTo(T bindable)
-        {
-            base.OnAttachedTo(bindable);
+	public class BindingContextPassingBehavior<T> : Behavior<T> where T : BindableObject
+	{
+		public T AssociatedObject { get; private set; }
+		
+		protected override void OnAttachedTo(T bindable)
+		{
+			base.OnAttachedTo(bindable);
 
-            AssociatedObject = bindable;
+			AssociatedObject = bindable;
 
-            if (bindable.BindingContext != null)
-            {
-                BindingContext = bindable.BindingContext;
-            }
-            bindable.BindingContextChanged += OnBindingContextChanged;
-        }
+			if (bindable.BindingContext != null)
+			{
+				BindingContext = bindable.BindingContext;
+			}
+			bindable.BindingContextChanged += OnBindingContextChanged;
+		}
 
-        protected override void OnBindingContextChanged()
-        {
-            base.OnBindingContextChanged();
+		protected override void OnBindingContextChanged()
+		{
+			base.OnBindingContextChanged();
 
-            BindingContext = AssociatedObject.BindingContext;
-        }
+			BindingContext = AssociatedObject.BindingContext;
+		}
 
-        private void OnBindingContextChanged(object sender, EventArgs e)
-        {
-            OnBindingContextChanged();
-        }
-    }
+		private void OnBindingContextChanged(object sender, EventArgs e)
+		{
+			OnBindingContextChanged();
+		}
+	}
 }
