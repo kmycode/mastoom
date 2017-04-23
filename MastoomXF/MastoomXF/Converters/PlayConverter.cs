82b9eb7944394ba8cb989d84cdb0cbcc210273dc
amay077 <okuokuoku@gmail.com>
2017-04-24 03:01:04 +0900

    「ｎ件の新着」を実装

---
+++ MastoomXF/MastoomXF/Converters/PlayConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Mastoom.Converters
+{
+	public class PlayConverter : IValueConverter
+	{
+		object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return "test";
+		}
+
+		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
