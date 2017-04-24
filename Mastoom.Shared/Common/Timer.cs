309c7910b3a3bc0744c253d26f88ddc63cb6261a
amay077 <okuokuoku@gmail.com>
2017-04-24 21:24:14 +0900

    ダミーの Timer 作った

--- Mastoom.Shared/Common/Timer.cs
+++ Mastoom.Shared/Common/Timer.cs
@@ -1,69 +1,10 @@
 ﻿using System;
-using System.Threading.Tasks;
-
-namespace Mastoom.Shared.Common
+namespace Mastoom.Shared
 {
-	/// <summary>
-	/// System.Threading.Timer クラスが Xamarin.Forms で使えないので分岐するためのラッパー
-	/// </summary>
-	/// <remarks>
-	/// Xamarin.Forms側は Task.Delay 使ってみたけど、これはプラットフォーム非依存だから、
-	/// 問題がなければ UWP 側も Task.Delay で共通化してもよいかも。
-	/// </remarks>
 	public class Timer
 	{
-#if WINDOWS_UWP
-        private readonly System.Threading.Timer _timer;
-
-        public Timer(Action<object> callback, object state, int dueTime, int period)
-        {
-            _timer = new System.Threading.Timer(s => callback?.Invoke(s), state, dueTime, period);
-        }
-
-        public bool Change(int dueTime, int period)
-        {
-            return _timer.Change(dueTime, period);
-        }
-#else
-		private int _dueTime;
-		private int _period;
-		public Timer(Action<object> callback, object state, int dueTime, int period)
+		public Timer()
 		{
-			_dueTime = dueTime;
-			_period = period;
-            StartTimer(callback, state);
 		}
-
-		void StartTimer(Action<object> callback, object state)
-		{
-			if (_dueTime < 0 || _period < 0)
-			{
-				return;
-			}
-
-			Task.Run(async () => 
-			{
-				await Task.Delay(_dueTime);
-			   	while(true)
-				{
-					callback?.Invoke(this);
-					await Task.Delay(_period);
-    			}
-			});
-		}
-
-		public bool Change(int dueTime, int period)
-		{
-			// マイナス値には替えられないようにしちゃったけど大丈夫かな？
-			if (dueTime < 0 || period < 0)
-			{
-				return false;
-			}
-
-			_dueTime = dueTime; // 変えても意味ないけど
-			_period = period;
-			return true;
-		}
-#endif
 	}
 }
