9053a82d804884eefdc82f4d1b7fed5238769640
amay077 <okuokuoku@gmail.com>
2017-04-24 21:25:05 +0900

    ダミーの Timer 作った

--- Mastoom.Shared/Common/Timer.cs
+++ Mastoom.Shared/Common/Timer.cs
@@ -1,10 +1,15 @@
 ﻿using System;
-namespace Mastoom.Shared
+namespace Mastoom.Shared.Common
 {
 	public class Timer
 	{
-		public Timer()
+		public Timer( Action<object> callback, object state, int dueTime, int period)
 		{
+		}
+
+		public bool Change(int dueTime, int period)
+		{
+			return true;
 		}
 	}
 }
