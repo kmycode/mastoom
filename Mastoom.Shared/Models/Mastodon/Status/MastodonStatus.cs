8851bfc5f51f6c702298b062a6dfba3cbf8789b7
amay077 <okuokuoku@gmail.com>
2017-04-24 22:33:04 +0900

    MediaAttachments と Tags を追加

--- Mastoom.Shared/Models/Mastodon/Status/MastodonStatus.cs
+++ Mastoom.Shared/Models/Mastodon/Status/MastodonStatus.cs
@@ -1,10 +1,12 @@
-﻿using Mastoom.Shared.Models.Mastodon.Account;
+﻿using Mastonet;
+using Mastoom.Shared.Common;
+using Mastoom.Shared.Models.Mastodon.Account;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
-using Mastoom.Shared.Parsers;
+using System.Threading.Tasks;
 using System.Linq;
 
 namespace Mastoom.Shared.Models.Mastodon.Status
@@ -12,8 +14,8 @@ namespace Mastoom.Shared.Models.Mastodon.Status
 	/// <summary>
 	/// Mastodonのステータス
 	/// </summary>
-	public class MastodonStatus : INotifyPropertyChanged
-	{
+    public class MastodonStatus : INotifyPropertyChanged
+    {
 		/// <summary>
 		/// ステータスのID
 		/// </summary>
@@ -24,42 +26,15 @@ namespace Mastoom.Shared.Models.Mastodon.Status
 		/// </summary>
 		public MastodonAccount Account { get; }
 
-		private IEnumerable<MastodonAttachment> _mediaAttachments;
-		public IEnumerable<MastodonAttachment> MediaAttachments
-		{
-			get { return _mediaAttachments; }
-			private set
-			{
-				_mediaAttachments = value;
-				this.OnPropertyChanged();
-			}
-		}
+        /// <summary>
+        /// これはブーストされた書き込みであるか
+        /// </summary>
+        public bool IsBoost { get; }
 
-		private IEnumerable<MastodonTag> _tags;
-		public IEnumerable<MastodonTag> Tags
-		{
-			get { return _tags; }
-			private set
-			{
-				_tags = value;
-				this.OnPropertyChanged();
-			}
-		}
-
-		private DateTime _createdAt;
-		public DateTime CreatedAt
-		{
-			get { return _createdAt; }
-			private set
-			{
-				if (_createdAt == value)
-				{
-					return;
-				}
-				_createdAt = value;
-				this.OnPropertyChanged();
-			}
-		}
+        /// <summary>
+        /// ブーストされたトゥートのID
+        /// </summary>
+        public int BoostedId { get; }
 
 		/// <summary>
 		/// ステータスの内容
@@ -81,21 +56,119 @@ namespace Mastoom.Shared.Models.Mastodon.Status
 		}
 		private string _content;
 
-		public MastodonStatus(int id, MastodonAccount account)
+        /// <summary>
+        /// ふぁぼったか
+        /// </summary>
+        public bool IsFavorited
+        {
+            get
+            {
+                return this._isFavorited;
+            }
+            private set
+            {
+                if (this._isFavorited != value)
+                {
+                    this._isFavorited = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+        private bool _isFavorited;
+
+        /// <summary>
+        /// 自分がこの書き込みをブーストしたか
+        /// </summary>
+        public bool IsBoosted
+        {
+            get
+            {
+                return this._isBoosted;
+            }
+            private set
+            {
+                if (this._isBoosted != value)
+                {
+                    this._isBoosted = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+        private bool _isBoosted;
+
+		/// <summary>
+		/// 添付メディア(画像など)群
+		/// </summary>
+		/// <value>The media attachments.</value>
+		public IEnumerable<MastodonAttachment> MediaAttachments
+		{
+			get { return _mediaAttachments; }
+			private set
+			{
+				_mediaAttachments = value;
+				this.OnPropertyChanged();
+			}
+		}
+		private IEnumerable<MastodonAttachment> _mediaAttachments;
+
+		/// <summary>
+		/// タグ群
+		/// </summary>
+		/// <value>The tags.</value>
+		public IEnumerable<MastodonTag> Tags
+		{
+			get { return _tags; }
+			private set
+			{
+				_tags = value;
+				this.OnPropertyChanged();
+			}
+		}
+		private IEnumerable<MastodonTag> _tags;
+
+		private DateTime _createdAt;
+		public DateTime CreatedAt
+		{
+			get { return _createdAt; }
+			private set
+			{
+				if (_createdAt == value)
+				{
+					return;
+				}
+				_createdAt = value;
+				this.OnPropertyChanged();
+			}
+		}
+
+        #region メソッド
+
+        public MastodonStatus(int id, MastodonAccount account)
 		{
 			this.Id = id;
 			this.Account = account;
 		}
-
-		private static int a = 0;
+        
 		public MastodonStatus(Mastonet.Entities.Status status)
-		{
-			this.Id = status.Id;
-			this.Content = status.Content;
+        {
+            // ブーストされたトゥートの場合でも、ブーストした人が取得したIDを設定する
+            // （元のトゥートとブースト分のトゥート、両方同じTLに表示するため）
+            this.Id = status.Id;
+
+            // ブーストされたトゥートの場合、ここから先はブースト対象のトゥートのデータを設定する
+            if (status.Reblog != null)
+            {
+                this.BoostedId = status.Reblog.Id;
+                status = status.Reblog;
+                this.IsBoost = true;
+            }
+
+            this.Content = status.Content;
+            this.IsFavorited = status.Favourited ?? false;
+            this.IsBoosted = status.Reblogged ?? false;
 			this.Account = new MastodonAccount(status.Account);
 			this.MediaAttachments = status.MediaAttachments.Select(x => new MastodonAttachment(x));
-			this.Tags = status.Tags.Select(x => new MastodonTag(x));
-			this.CreatedAt = status.CreatedAt;
+            this.Tags = status.Tags.Select(x => new MastodonTag(x));
 		}
 
 		public void CopyTo(MastodonStatus to)
@@ -105,14 +178,71 @@ namespace Mastoom.Shared.Models.Mastodon.Status
 				throw new InvalidOperationException("コピーしようとするStatusのIDが違います");
 			}
 			to.Content = this.Content;
-			to.MediaAttachments = this.MediaAttachments.Select(x => x);
-			to.Tags = this.Tags.Select(x => x);
-			to.CreatedAt = this.CreatedAt;
+            to.IsFavorited = this.IsFavorited;
+            to.IsBoosted = this.IsBoosted;
+            to.MediaAttachments = this.MediaAttachments.Select(x => x); // なんとなくIEnumerableのガワだけ生成しなおし
+            to.Tags = this.Tags.Select(x => x); // なんとなくIEnumerableのガワだけ生成しなおし
 		}
 
-		#region INotifyPropertyChanged
+        #endregion
 
-		public event PropertyChangedEventHandler PropertyChanged;
+        #region サーバと通信し状態を操作するメソッド
+
+        /// <summary>
+        /// ふぁぼとあんふぁぼを切り替える
+        /// </summary>
+        /// <param name="client">クライアント</param>
+        /// <returns>成功したかどうか</returns>
+        public async Task<bool> ToggleFavoriteAsync(MastodonClient client)
+        {
+            bool isSucceed = false;
+            try
+            {
+                var newStatus = new MastodonStatus(
+                    !this.IsFavorited ? await client.Favourite(this.Id) : await client.Unfavourite(this.Id)
+                    );
+                isSucceed = true;
+            }
+            catch { }
+
+            if (isSucceed)
+            {
+                this.IsFavorited ^= true;
+            }
+
+            return isSucceed;
+        }
+
+        /// <summary>
+        /// ブーストとアンブーストを切り替える
+        /// </summary>
+        /// <param name="client">クライアント</param>
+        /// <returns>成功したかどうか</returns>
+        public async Task<bool> ToggleBoostAsync(MastodonClient client)
+        {
+            bool isSucceed = false;
+            try
+            {
+                var newStatus = new MastodonStatus(
+                    !this.IsBoosted ? await client.Reblog(this.Id) : await client.Unreblog(this.Id)
+                    );
+                isSucceed = true;
+            }
+            catch { }
+
+            if (isSucceed)
+            {
+                this.IsBoosted ^= true;
+            }
+
+            return isSucceed;
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
 		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
 		{
 			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
