4dcd1823a8bd4609df8d486596cc0fc460ddeea1
amay077 <okuokuoku@gmail.com>
2017-04-30 17:20:00 +0900

    Repositoryクラス化

--- Mastoom.Shared/Models/Mastodon/Connection/MastodonConnectionGroupCollection.cs
+++ Mastoom.Shared/Models/Mastodon/Connection/MastodonConnectionGroupCollection.cs
@@ -1,47 +1,58 @@
-﻿using System;
+﻿using Mastoom.Shared.Models.Mastodon.Connection.Function.Container;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using Mastoom.Shared.Repositories;
 
 namespace Mastoom.Shared.Models.Mastodon.Connection
 {
     public class MastodonConnectionGroupCollection : ObservableCollection<MastodonConnectionGroup>
     {
+        private readonly OAuthAccessTokenRepository tokenRepo = new OAuthAccessTokenRepository();
 #if DEBUG
         public void AddTestConnection()
         {
-            //this.Add(new MastodonConnectionGroup
-            //{
-            //    Name = "Pawoo",
-            //});
-            //this[0].TryAdd(new MastodonConnection("pawoo.net", ConnectionType.HomeTimeline)
-            //{
-            //    Name = "ホーム",
-            //});
-            //this[0].TryAdd(new MastodonConnection("pawoo.net", ConnectionType.LocalTimeline)
-            //{
-            //    Name = "ローカルタイムライン",
-            //});
-            //this[0].TryAdd(new MastodonConnection("pawoo.net", ConnectionType.PublicTimeline)
-            //{
-            //    Name = "公開タイムライン",
-            //});
+            this.Add(new MastodonConnectionGroup
+            {
+                Name = "Pawoo",
+            });
+            this[0].TryAdd(new MastodonConnection("pawoo.net", new HomeTimelineFunctionContainer(), tokenRepo)
+            {
+                Name = "ホーム",
+            });
+            this[0].TryAdd(new MastodonConnection("pawoo.net", new NotificationFunctionContainer(), tokenRepo)
+            {
+                Name = "通知",
+            });
+            this[0].TryAdd(new MastodonConnection("pawoo.net", new LocalTimelineFunctionContainer(), tokenRepo)
+            {
+                Name = "ローカルタイムライン",
+            });
+            this[0].TryAdd(new MastodonConnection("pawoo.net", new PublicTimelineFunctionContainer(), tokenRepo)
+            {
+                Name = "公開タイムライン",
+            });
 
             this.Add(new MastodonConnectionGroup
             {
                 Name = "Mstdn",
             });
-            //this[1].TryAdd(new MastodonConnection("mstdn.jp", ConnectionType.HomeTimeline)
-            //{
-            //    Name = "ホーム",
-            //});
-            //this[1].TryAdd(new MastodonConnection("mstdn.jp", ConnectionType.LocalTimeline)
-            //{
-            //    Name = "ローカルタイムライン",
-            //});
-            this[0].TryAdd(new MastodonConnection("mstdn.jp", ConnectionType.PublicTimeline)
+            this[1].TryAdd(new MastodonConnection("mstdn.jp", new HomeTimelineFunctionContainer(), tokenRepo)
+            {
+                Name = "ホーム",
+            });
+            this[1].TryAdd(new MastodonConnection("mstdn.jp", new NotificationFunctionContainer(), tokenRepo)
+            {
+                Name = "通知",
+            });
+            this[1].TryAdd(new MastodonConnection("mstdn.jp", new LocalTimelineFunctionContainer(), tokenRepo)
+            {
+                Name = "ローカルタイムライン",
+            });
+            this[1].TryAdd(new MastodonConnection("mstdn.jp", new PublicTimelineFunctionContainer(), tokenRepo)
             {
                 Name = "公開タイムライン",
             });
