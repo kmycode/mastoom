80840ba1c848bb8251b89d8e838563cb17d42d5f
amay077 <okuokuoku@gmail.com>
2017-04-30 17:15:45 +0900

    Repositoryクラス化

---
+++ Mastoom.Shared/Repositories/OAuthAccessTokenRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Akavache;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+
+namespace Mastoom.Shared.Repositories
+{
+	public class OAuthAccessTokenRepository
+	{
+		public OAuthAccessTokenRepository()
+		{
+		}
+
+        public Task Save(string instanceUri, string accessToken)
+        {
+            return BlobCache.LocalMachine.InsertObject(instanceUri, accessToken).ToTask().ContinueWith(_ => 
+            {
+                BlobCache.LocalMachine.Flush();
+            });
+        }
+
+        internal Task<string> Load(string instanceUri)
+        {
+            return BlobCache.LocalMachine.GetOrCreateObject<string>(instanceUri, () => string.Empty).ToTask();
+        }
+    }
+}
